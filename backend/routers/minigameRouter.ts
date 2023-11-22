ae9e4846af44305e5590cf77f10c707e20520c27
sStieg <stiegjosef@gmail.com>
2023-11-22 11:30:16 +0100

    Changed project structure, added quarkus backend

--- backend/routers/minigameRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { join } from "path";
-
-export const minigameRouter = express.Router();
-
-minigameRouter.use("/", minigameRouter.use(express.static(join(__dirname, "../../frontend"))))
-
-minigameRouter.use("/1", minigameRouter.use(express.static(join(__dirname, "../../frontend/games/Projekt_Grillparty"))));
