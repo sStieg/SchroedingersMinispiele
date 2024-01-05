bc0994aced1fcb805b25b45f67e41ed07bafb818
sStieg <stiegjosef@gmail.com>
2024-01-05 23:06:13 +0100

    #12 adjusted RestAPI.java and application.properties

--- backend/src/main/java/at/schroedingers/minigames/RestAPI.java
+++ backend/src/main/java/at/schroedingers/minigames/RestAPI.java
@@ -5,19 +5,27 @@ import jakarta.websocket.Session;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.net.URI;
 
-@Path("/api")
+@Path("/position")
 public class RestAPI {
 
     @GET
-    @Path("/position/{position}")
+    @Path("/{position}")
     public void startGame(@PathParam("position") int position) throws Exception {
 
         try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(WebSocketClient.class, URI.create("http://localhost:8080/connect-websocket/rest"))) {
             session.getAsyncRemote().sendText(String.format("%d" ,position));
         }
 
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWorld() {
+        return "hello, world";
     }
 }
