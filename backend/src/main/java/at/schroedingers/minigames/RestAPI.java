4ca845e3b52bd5f3d27331a0f4f0aba68e185be1
sStieg <stiegjosef@gmail.com>
2024-01-07 23:47:45 +0100

    moved javascript files

--- backend/src/main/java/at/schroedingers/minigames/RestAPI.java
+++ backend/src/main/java/at/schroedingers/minigames/RestAPI.java
@@ -2,10 +2,7 @@ package at.schroedingers.minigames;
 
 import jakarta.websocket.ContainerProvider;
 import jakarta.websocket.Session;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.net.URI;
@@ -13,10 +10,11 @@ import java.net.URI;
 @Path("/position")
 public class RestAPI {
 
-    @GET
-    @Path("/{position}")
-    public void startGame(@PathParam("position") int position) throws Exception {
-
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void startGame(Object position) throws Exception {
+        System.out.println(position);
         try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(WebSocketClient.class, URI.create("http://localhost:8080/connect-websocket/rest"))) {
             session.getAsyncRemote().sendText(String.format("%d" ,position));
         }
---
+++ frontend/javascript/script.js
@@ -0,0 +1,26 @@
+var connected = false;
+var socket;
+$(document).ready(function () {
+    connect();
+});
+var connect = function () {
+    if (!connected) {
+        socket = new WebSocket("ws://localhost:8080/connect-websocket/player1");
+        socket.onopen = function () {
+            connected = true;
+            console.log("Connected to the web socket");
+        };
+        socket.onmessage = function (m) {
+            console.log(m.data);
+            if (m.data.toString() == '1') {
+                startGame(m.data);
+            }
+        };
+    }
+};
+function startGame(gameNumber) {
+    if (gameNumber == 1) {
+        $('#minigame').css("opacity", "1");
+        $('#pacman-game').css("opacity", "1");
+    }
+}
