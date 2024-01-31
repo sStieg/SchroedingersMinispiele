f90ac3733c58163d78c39349f10f91b646119e09
sStieg <stiegjosef@gmail.com>
2024-01-31 12:26:58 +0100

    #24 added mock vr headset site for testing

---
+++ frontend/test/script.js
@@ -0,0 +1,33 @@
+var connected = false;
+        var socket;
+
+        $( document ).ready(function() {
+            connect();
+        });
+
+        var connect = function() {
+            if (!connected) {
+                socket = new WebSocket("ws://schroedinger.hopto.org/api/connect-websocket/vrplayer");
+                socket.onopen = function () {
+                    connected = true;
+                    console.log("Connected to the web socket");
+                };
+
+                socket.onmessage = function (m) {
+                    console.log(m.data);
+
+                    if(m.data.toString().includes("solution")) {
+                        $('#solution').html(m.data);
+                    }
+                };
+            }
+        }
+
+        function startGame() {
+            let data = {"position": 1};
+            fetch("http://schroedinger.hopto.org/api/position/", {
+                method: "POST",
+                mode: "no-cors",
+                body: 2
+            })
+        }
