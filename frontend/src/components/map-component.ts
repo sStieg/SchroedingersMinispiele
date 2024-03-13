fb102be649164134ec2a150e20f76fa371246f62
sStieg <stiegjosef@gmail.com>
2024-03-13 12:26:32 +0100

    #35 named rooms in map-component

--- frontend/src/components/map-component.ts
+++ frontend/src/components/map-component.ts
@@ -5,10 +5,10 @@ console.log("map-component")
 
 const template = () => html`
 <div>
-    <room-component class="room" id="room1">Room 1</room-component>
-    <room-component class="room" id="room2">Room 2</room-component>
-    <room-component class="room" id="room3">Room 3</room-component>
-    <room-component class="room" id="room4">Room 4</room-component>
+    <room-component class="room" id="room1">Kinderzimmer</room-component>
+    <room-component class="room" id="room2">Bibliothek</room-component>
+    <room-component class="room" id="room3">Labor</room-component>
+    <room-component class="room" id="room4">Kerker</room-component>
 </div>
 `
 
