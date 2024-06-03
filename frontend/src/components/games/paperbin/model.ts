fced704a5c8df346593c87259b63d19457f25e32
maschenberger <maria.aschenberger@gmx.net>
2024-06-03 22:43:53 +0200

    #41 object oriented with class

--- frontend/src/components/games/paperbin/model.ts
+++ frontend/src/components/games/paperbin/model.ts
@@ -1,6 +1,8 @@
 import{BehaviorSubject} from 'rxjs'
 
 interface GameElement {
+    elementId: string
+    position: Rectangle
     move: (dx: number, dy: number) => void
 }
 
@@ -9,7 +11,7 @@ interface Point {
     y: number
 }
 
-interface Rectangle {
+class Rectangle {
     center: Point
     width: number
     height: number
@@ -41,8 +43,7 @@ export class PaperPlane implements GameElement{
         this.position.center.x = x;
         this.position.center.y = y;  
     }
-
-    id: number
+    id: number = 0
     position: Rectangle = {
         center: {
             x: 0,
@@ -51,35 +52,13 @@ export class PaperPlane implements GameElement{
         width: 20,
         height: 20
     }
-    velocityX: number
-    velocityY: number
-    transform: string
-    state: PaperPlaneState
-    htmlElement: HTMLElement
+    velocityX: number = 0
+    velocityY: number = 0
+    elementId: string = ""
 }
 
 export class Bin {
-    move(dx: number, dy: number) {
-        // current position
-        let x = this.position.center.x;
-        let y = this.position.center.y;
-                
-        // calc new position
-        x += dx;
-        y += dy;
-    
-        if(x < 0) {
-            x = 0;
-        } else if (x > 95) {
-            x = 95;
-        }
-                
-        // assign new position
-        this.position.center.x = x;
-        this.position.center.y = y;  
-    }	
-
-    id: string
+    move(dx: number, dy: number){}	
     position: Rectangle = {
         center: {
             x: 0,
@@ -88,7 +67,16 @@ export class Bin {
         width: 20,
         height: 20
     }
+    id: string
     width: number
+    elementId: string
+}
+
+export class PaperBinGame {
+    running: boolean
+    isWon: boolean
+    paperPlanes: PaperPlane[]
+    bin: Bin
 }
 
 export interface PaperBinGameState {
@@ -101,4 +89,42 @@ const initalState: PaperBinGameState = {
     bin: undefined
 }
 
-export const store = new BehaviorSubject<PaperBinGameState>(initalState)
+export const paperbinGame: PaperBinGame = {
+    running: false,
+    isWon: false,
+    paperPlanes: [],     
+    bin: {
+        move(dx: number, dy: number){
+            // current position
+            let x = this.position.center.x;
+            let y = this.position.center.y;
+                    
+            // calc new position
+            x += dx;
+            y += dy;
+        
+            if(x < 0) {
+                x = 0;
+            } else if (x > 95) {
+                x = 95;
+            }
+                    
+            // assign new position
+            this.position.center.x = x;
+            this.position.center.y = y;  
+        },
+        id: "",
+        position: {
+            center: {
+                x: 0,
+                y: 0
+            },
+            width: 20,
+            height: 20
+        },
+        width: 0,
+        elementId: "bin"
+    }
+}
+
+export const gameState = new BehaviorSubject<PaperBinGameState>(paperbinGame)
--- frontend/src/components/games/paperbin/paper-bin-component.ts
+++ frontend/src/components/games/paperbin/paper-bin-component.ts
@@ -1,37 +1,176 @@
 import { html, render } from "lit-html";
-import { PaperBinGameState, PaperPlane, Bin, store } from "./model";
+import { PaperBinGameState, PaperPlane, Bin, gameState, paperbinGame} from "./model";
 import {produce} from 'immer'
 import {endGame} from "../../../script";
 
-/* INIT */
-let gameInterval: NodeJS.Timeout;
-let planes: PaperPlane[];
-let gameState: PaperBinGameState = {
-    paperPlanes: [],
-    bin: new Bin
-}
-
-let leftArrow = false;
-let rightArrow = false;
-
-let planeId = 0;
-let planeCounter = 0;
-
 
 class PaperBinComponent extends HTMLElement{
+    gameInterval: NodeJS.Timeout;
+    planeInterval: NodeJS.Timeout;
+    planes: PaperPlane[];
+    leftArrow = false;
+    rightArrow = false;
+
+    planeId = 0;
+    planeCounter = -1;
+    isWon: boolean = false;
+
     connectedCallback(){
         console.log("connected")
-        /*store.subscribe(paperbingame => {
-            this.render(paperbingame)
-        })*/
-        this.render(gameState)
+        gameState.subscribe(() => {
+            this.render()
+        })
+        this.startPaperBinGame();
     }
-    render(paperbingame: PaperBinGameState) {
-        render(template(paperbingame), this);
+    render() {
+        render(template(), this);
     }
 
+    checkKorb(plane: PaperPlane) { //plane übergeben 
+        if(plane != null && paperbinGame.paperPlanes[plane.id] != null){
+            if((paperbinGame.paperPlanes[plane.id].position.center.x >= paperbinGame.bin.position.center.x-2) && (paperbinGame.paperPlanes[plane.id].position.center.x <= paperbinGame.bin.position.center.x+2) &&
+            (paperbinGame.paperPlanes[plane.id].position.center.y <= paperbinGame.bin.position.center.y+2)){
+                //clearInterval(gameInterval);
+                this.deletePlane(plane.id)
+                this.planeCounter++;
+                console.log(this.planeCounter)
+    
+                if(this.planeCounter === 0){
+                    console.log("YOU WON")
+                    this.isWon = true;
+                    endGame();
+                }
+            }
+        }
+    }
 
+    startPaperBinGame() {
+        //bin initialisieren
+        paperbinGame.bin.position.center.x = 5;
+        paperbinGame.bin.position.center.y = 99;
+        paperbinGame.bin.id = 'sprite';
+        console.log("initialized bin")
 
+        //plane array initialisieren
+        this.planes = paperbinGame.paperPlanes;
+        console.log(this.planes)
+
+        this.gameInterval = setInterval(() => {
+            this.gameLoop()
+        }, 10); // async recursion
+
+        this.planeInterval = setInterval(() => {
+            this.spawnPlane()
+        }, 2000); 
+
+        document.onkeydown = (e) => {
+            this.keyListenerDown(e);
+        }
+        document.onkeyup = (e) => {
+            this.keyListenerUp(e);
+        }
+    }
+
+    gameLoop() {
+        if(paperbinGame.running){
+            for (let i = 0; i < this.planes.length; i++) {
+                this.checkKorb(paperbinGame.paperPlanes[i]);
+            }
+        
+            if(this.leftArrow) {
+                this.moveBin(-0.5, 0);
+            }
+            if(this.rightArrow) {
+                this.moveBin(0.5, 0);
+            }
+            
+            this.planes.forEach(pl => this.movePlane(pl));
+        } 
+
+        this.render();
+    }
+
+    
+    keyListenerDown(e){        
+        //console.log(e.keyCode);
+        if (!e){
+            e = window.event; //Internet Explorer
+        }
+        if (e.keyCode == 37){ // leftArrow
+            this.leftArrow = true;
+        }
+        if (e.keyCode == 39){ // rightArrow
+            this.rightArrow = true;
+        }
+    }
+
+    keyListenerUp(e){
+        //console.log(e);
+        //console.log(e.keyCode);
+        if (!e){
+            e = window.event; //Internet Explorer
+        }
+        if (e.keyCode == 37){ // leftArrow
+            this.leftArrow = false;
+        }
+        if (e.keyCode == 39){ // rightArrow
+            this.rightArrow = false;
+        }
+    }
+
+    moveBin(dx: number, dy:number){
+        paperbinGame.bin.move(dx, dy);
+    }
+    
+    movePlane(pl: PaperPlane){
+        pl.move(pl.velocityX, pl.velocityY);
+    
+        if(pl.position.center.y > 135 || pl.position.center.x <= 10 || pl.position.center.x >= 95){
+            this.deletePlane(pl.id)
+        }else{
+            pl.move(pl.velocityX, pl.velocityY);
+        } 
+    }
+
+    deletePlane(id: number){
+        for (let i = 0; i < this.planes.length; i++) {
+            if(this.planes[i].id === id){
+                this.planes.splice(i,1)
+            }   
+        }
+    }
+
+    spawnPlane(){
+        if(paperbinGame.running){
+            const plane = this.createPlane(this.randomPositionX(), 0, 0.05, this.randomSpeed());
+            paperbinGame.paperPlanes.push(plane);
+        }  
+    }
+    
+    createPlane(x: number, y: number, dx: number, dy: number): PaperPlane{
+        let plane = new PaperPlane()
+
+        plane.position.center.x = x;
+        plane.position.center.y = y;
+        plane.id = this.planeId;
+        this.planeId++;
+        plane.velocityX = dx;
+        plane.velocityY = dy;
+            
+        if(x>50){
+            plane.velocityX =- plane.velocityX;
+        }
+
+        return plane;
+    }
+
+    randomPositionX(){
+        return Math.floor(Math.random() * 100)
+    } 
+    
+    randomSpeed(){
+        return Math.random() * (0.5 - 0.001) + 0.001; //random number zwischen 0.001 und 0.5
+    }
     /*
     tick(){
         const nextState = produce(store.getValue(), game => {
@@ -42,32 +181,71 @@ class PaperBinComponent extends HTMLElement{
 }
 customElements.define("paper-bin-component", PaperBinComponent);
 
-function template(paperbingame: PaperBinGameState) {
-    console.log("template", paperbingame)
+function template() {
     //TODO: const planeBoxes = paperbinGame.planes.map(plane => createPlaneBox(plane))
+    const binStyle = 
+        "left:" + paperbinGame.bin.position.center.x + "%;" +
+        "top:" + paperbinGame.bin.position.center.y + "%";
+        
+    let planesHTML = [];
+    
+    for (let i = 0; i < paperbinGame.paperPlanes.length; i++) {
+        planesHTML[i] = html`
+        <div style="position: absolute; left: ${paperbinGame.paperPlanes[i].position.center.x}%; top:${paperbinGame.paperPlanes[i].position.center.y}%;">
+            <img id="plane" style="width: 5vw; height: auto; position: absolute; transform: rotate(40deg)" src="../../../images/papierflieger.png"/>
+        </div>`; 
+    }
+    
     return html`
     <style>
-        
         #surface{
             width: 50vw;
             height: 50vh;
             position: relative;
         }
+
+        #sprite{
+            position: absolute;
+            z-index: 100;
+        }
+
+        #winning #button {
+            width: 10%;
+            height: 30px;
+            color: #fff;
+            background-color: #5a8d23;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
     </style>
     <div id="paperBinGame" class="game">
         <div id="surface">
-            <div id="sprite" style="position: absolute; bottom: 20vw; left: 20vw; z-index: 100;">
+            <div id="sprite" style=${binStyle}>
                 <img id="korb" style="width: 15vw; height: auto" src = "../../../images/papierkorb.png"/>
-            </div>  
-            <!-- $ {planeBoxes}
-            -->
+            </div>
+            ${planesHTML}
+
+            <div id="winning" ?hidden=${paperbinGame.isWon}>
+                <h1>Gewonnen!</h1>
+                <div id="button">Fertig</div>
+            </div>
         </div>
     </div>
     `
 }
 
+export function startPaperBinGame() {
+    paperbinGame.running = true;
+}
+
+document.getElementById("button").addEventListener("click", () => {
+    endGame();
+})
+
 
 /* INIT GAME */
+/*
 let surface = document.getElementById("surface");
 
 export function startPaperBinGame() {
@@ -135,6 +313,7 @@ function randomSpeed(){
 }
         
 /* CHECK PRESSED KEY */
+/*
 function keyListenerDown(e){        
     //console.log(e.keyCode);
     if (!e){
@@ -164,6 +343,7 @@ function keyListenerUp(e){
 
 
 /* GAME LOOP */
+/*
 function gameLoop() {
     for (let i = 0; i < planes.length; i++) {
         checkKorb(gameState.paperPlanes[i]);
@@ -232,4 +412,4 @@ function checkKorb(plane: PaperPlane) { //plane übergeben
                 endGame();
             }
     }
-}
+}*/
