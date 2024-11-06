7344221739099b73534032da279b00cd6c2ea1d3
maschenberger <maria.aschenberger@gmx.net>
2024-11-06 22:10:37 +0100

    #47 include actual diary

--- frontend/src/components/diary-component.ts
+++ frontend/src/components/diary-component.ts
@@ -4,13 +4,7 @@ console.log("diary-component")
 
 const template = () => html`
 <div>
-<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore 
-    magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
-    et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-    no sea takimata et ea rebum. Stet clita kasd gubergren, 
-    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem 
-    ipsum dolor sit amet...</p>
+<p>Als Kind mochte ich schon immer Rätsel. Ich hatte sie alle! Sie waren überall in den Zeitschriften verstreut, die ich jeden Monat bekam. Da saß ich also ganz allein auf dem Boden meines Schlafzimmers.\n\nEs ist komisch, dass ich mich gerade erst daran erinnere. Die Jahre vergingen und als ich berühmt wurde, schien meine Einsamkeit einfach zu verschwinden. Die einzigen Stunden, die ich wirklich alleine verbrachte, waren, wenn ich schlief oder an meinen Arbeiten arbeitete, aber in diesen Zeiten fühlte ich mich nicht einsam.\n\nMeine Wissenschaft leistete mir immer gute Gesellschaft. Oft vergingen Tage, bis mir klar wurde, dass ich weder geschlafen noch gegessen hatte. Es gab nur meine Bücher, meine Schreibmaschine und mich.\nDiese Werke sollten mich berühmt machen, und das taten sie auch. Ein Werk nach dem anderen gewann Preise. Kein Wunder, schließlich habe ich nicht nur das geschrieben, was ich aus Büchern wusste, wie all diese Möchtegern-Wissenschaftler, sondern hatte auch zwei geniale Strategien.\n\n1. Fassen Sie das Gelernte in einem völlig neuen Konzept zusammen.\n2. Versuchen Sie es, versuchen Sie es.\nIch habe alle meine Experimente selbst durchgeführt. Die Blätter auf meinem Tisch waren gefüllt mit Skizzen, Daten und Fakten. Es lief nicht immer alles nach Plan, das musste ich zugeben, aber was wäre schon eine kleine Explosion im Keller, wenn einem nicht das Dach auf den Kopf fiel?\n\nHeute stand ich also wieder hier, vor dem Kinderzimmer. Das Haus meiner Eltern war nicht besonders groß, nicht besonders protzig, aber es war ein Zuhause. Die Zimmer habe ich kaum genutzt, die meiste Zeit habe ich in Hotels am anderen Ende der Stadt geschlafen und als ich zurückkam, war es einsam wie immer. Manchmal stelle ich mir vor, ihre Stimmen zu hören, wenn ich daran denke, das Haus zu verkaufen.\nAber Mutter! Keine Sorge, ich bin noch nicht bereit.</p>
 </div>
 `
 
