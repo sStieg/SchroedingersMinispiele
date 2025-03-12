3e73008f85b93febb7600f23c642f9a1fdb3d58d
sStieg <stiegjosef@gmail.com>
2025-03-12 15:23:11 +0100

    changes

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,80 +1,104 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
-  return {
-    mode: isProduction ? "production" : "development",
-    entry: {
-      main: "./frontendIF/src/js/script.js",
-    },
-    output: {
-      filename: "[name]-build.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-      publicPath: "/",
-    },
-    devtool: isProduction ? "source-map" : "eval-source-map",
-    devServer: {
-      static: {
-        directory: path.join(__dirname, "dist"),
-      },
-      watchFiles: ["frontendIF/src/**/*"],
-      open: "/login-lobby.html",
-      host: "localhost",
-      port: 4200,
-      hot: true,
-      historyApiFallback: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: "login-lobby.html",
-        template: "./login-lobby/login-lobby.html",
-      }),
-      new HtmlWebpackPlugin({
-        filename: "how-to-play.html",
-        template: "./frontendIF/src/pages/how-to-play.html",
-      }),
-      new HtmlWebpackPlugin({
-        filename: "about-us.html",
-        template: "./frontendIF/src/pages/about-us.html",
-      }),
-      new MiniCssExtractPlugin({
-        filename: "css/[name].css",
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: "frontendIF/src/css", to: "css" },
-          { from: "frontendIF/src/js", to: "js" },
-          { from: "frontendIF/src/pages/navbar.html", to: "navbar.html" },
-        ],
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-        {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config = {
+  entry: {
+    main: './src/index.ts',
+    test: './test/index.ts',
+    'login-lobby': './login-lobby/login-lobby.js',
+    'login-lobby': './frontendIF/src/js/script.js',
+    'login-user': './login-user/login-user.js',
+  },
+  output: {
+    filename: '[name]-build.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    open: '/login-lobby.html',
+    host: 'localhost',
+    port: 4200,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'main.html',
+      template: './index.html',
+      chunks: ['main'],
+      hash: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'test.html',
+      template: './test/index.html',
+      chunks: ['test'],
+      hash: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login-lobby.html',
+      template: './login-lobby/login-lobby.html',
+      chunks: ['login-lobby'],
+      hash: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login-user.html',
+      template: './login-user/login-user.html',
+      chunks: ['login-user'],
+      hash: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: "how-to-play.html",
+      template: "./frontendIF/src/pages/how-to-play.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "about-us.html",
+      template: "./frontendIF/src/pages/about-us.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'scss', to: 'scss' },
+        { from: 'images', to: 'images' },
+        { from: 'test/images', to: 'images' },
       ],
-    },
-    resolve: {
-      extensions: [".js"],
-    },
-    performance: {
-      maxAssetSize: 300000, 
-      maxEntrypointSize: 300000, 
-      hints: isProduction ? "warning" : false, 
-    },
-  };
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
