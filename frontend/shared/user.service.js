a382121821cb4b31b948d125f2f018107ed28321
sStieg <stiegjosef@gmail.com>
2025-04-02 18:06:06 +0200

    updated url to ahif backend

--- frontend/shared/user.service.js
+++ frontend/shared/user.service.js
@@ -1,110 +1,28 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+import { BASE_URL } from './util.js';
+
+
+const USER_API_URL = `https://${BASE_URL}/user`;
+
+/**
+ * Attempt to log the user in.
+ * @param {string} username - The username to log in with.
+ * @param {string} lobbyId - The lobby ID to log in to.
+ * @returns {Promise<boolean>} - Resolves to `true` if login succeeds, otherwise `false`.
+ */
+export async function attemptUserLogin(username, lobbyId) {
+  try {
+    const response = await fetch(USER_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userName: username, lobbyId: lobbyId }),
     });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserService = void 0;
-var util_1 = require("./util");
-var URL = "https://".concat(util_1.BASE_URL, "/user");
-var UserService = /** @class */ (function () {
-    function UserService() {
-        this.userName = null;
-        this.lobbyId = null;
-    }
-    // Attempt user login
-    UserService.prototype.attempUserLogin = function (userNameInput, lobbyId) {
-        return __awaiter(this, void 0, void 0, function () {
-            var response, result, error_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        _a.trys.push([0, 3, , 4]);
-                        return [4 /*yield*/, fetch(URL, {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({ userNameInput: userNameInput, lobbyId: lobbyId }),
-                            })];
-                    case 1:
-                        response = _a.sent();
-                        if (!response.ok) {
-                            console.error("Failed to log in user", response.status);
-                            return [2 /*return*/, false];
-                        }
-                        return [4 /*yield*/, response.json()];
-                    case 2:
-                        result = _a.sent();
-                        console.log(result.success);
-                        if (result.success) {
-                            this.userName = userNameInput;
-                            this.lobbyId = lobbyId;
-                            return [2 /*return*/, true];
-                        }
-                        return [2 /*return*/, false];
-                    case 3:
-                        error_1 = _a.sent();
-                        console.error("Error logging in user:", error_1);
-                        return [2 /*return*/, false];
-                    case 4: return [2 /*return*/];
-                }
-            });
-        });
-    };
-    Object.defineProperty(UserService.prototype, "user", {
-        // Getters for user and lobby IDs
-        get: function () {
-            return this.userName;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(UserService.prototype, "lobby", {
-        get: function () {
-            return this.lobbyId;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return UserService;
-}());
-exports.UserService = UserService;
-// Example usage
-/*(async () => {
-  const userService = new UserService();
-  const loginSuccess = await userService.attempUserLogin("TestUser", "12345");
-  console.log("Login Success:", loginSuccess);
-})();*/ 
+
+    const result = await response.json();
+    return result.success;
+  } catch (error) {
+    console.error('Login error:', error);
+    return false;
+  }
+}
